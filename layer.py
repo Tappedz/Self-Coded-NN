201e80588c8cb24a0e3109673a037e0ef8918131
Tappedz <80057934+Tappedz@users.noreply.github.com>
2024-01-09 13:52:07 +0100

    Backpropagation & params update implemented

--- layer.py
+++ layer.py
@@ -2,23 +2,24 @@ import numpy as np
 
 class Layer:
     output_layer: bool
+    n_neurons: int
 
     def __init__(self, n_inputs, n_neurons, output_layer):
-        self.weights = np.random.rand(n_inputs, n_neurons)
-        self.biases = np.random.rand(1, n_neurons)
+        self.weights = np.random.rand(n_neurons, n_inputs)
+        self.biases = np.random.rand(n_neurons, 1)
+        self.n_neurons = n_neurons
         self.output_layer = output_layer
 
     def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
+        self.z = np.dot(self.weights, inputs) + self.biases
         if not self.output_layer:
-            self.output = self.ReLU_activation(self.output)
+            self.ReLU_activation(self.z)
         else:
-            self.output = self.softmax(self.output)
+            self.softmax(self.z)
 
     def ReLU_activation(self, inputs):
-        self.output = np.maximum(0, inputs)
+        self.a = np.maximum(0, inputs)
 
-    def softmax(self, inputs):
-        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-        self.output = probabilities
+    def softmax(self, inputs): #axis=0 por columnas, axis=1 por filas
+        self.a = (np.exp(inputs - np.max(inputs, axis=0, keepdims=True)) 
+                  / np.sum(np.exp(inputs - np.max(inputs, axis=0, keepdims=True)), axis=0, keepdims=True))
--- neural_network.py
+++ neural_network.py
@@ -2,27 +2,30 @@ import numpy as np
 from layer import Layer
 
 class NeuralNetwork:
-    n_hid_layers: int
+    n_layers: int
     n_neurons: int
-    layers: []
 
-
-    def __init__(self, n_hid_layers, n_neurons, n_inputs, n_outputs):
-        self.n_hid_layers = n_hid_layers
+    def __init__(self, n_layers, n_neurons, n_inputs, n_outputs):
+        self.n_layers = n_layers
         self.n_neurons = n_neurons
+        self.layers = []
 
-        for i in range(n_hid_layers):
+        for i in range(n_layers - 1):
             if i == 0:
-                self.layers.append(Layer(n_inputs, n_neurons))
+                self.layers.append(Layer(n_inputs, n_neurons, False))
             else:
-                self.layers.append(Layer(n_neurons, n_neurons))
-        self.layers.append(Layer(n_neurons, n_outputs))
+                self.layers.append(Layer(n_neurons, n_neurons, False))
+        self.layers.append(Layer(n_neurons, n_outputs, True))
 
-    def forward(self, inputs):
+    def forward(self, inputs): #inputs n_batches x n_inputs
+        self.inputs = inputs.T
         for i in range(len(self.layers)):
-            self.layers[i].forward(inputs)
-            inputs = self.layers[i].output
-        return inputs
+            if i == 0:
+                self.layers[i].forward(self.inputs)
+            else:
+                self.layers[i].forward(fwd)
+            fwd = self.layers[i].a
+        return fwd
 
     def calculate_loss(self, predictions, labels): # Cross Entropy Loss
         samples = len(predictions)
@@ -31,14 +34,39 @@ class NeuralNetwork:
         if len(labels.shape) == 1: # scalars
             confidences = y_pred_clipped[range(samples), labels]
         elif len(labels.shape) == 2: # one-hot encoding
-            confidences = np.sum(y_pred_clipped*labels, axis=1)
+            confidences = np.sum(y_pred_clipped*labels, axis=0)
 
         return -np.log(confidences)
+    
+    def calculate_accuracy(self, predictions, labels):
+        pred_indexes = np.argmax(predictions, axis=0)
+        labels_indexes = np.argmax(labels, axis=0)
+        return (np.count_nonzero(pred_indexes == labels_indexes) / predictions.shape[1])
 
-
-    def backpropagation(self, loss):
-        for i in range(len(self.layers) - 1, 0, -1):
-            self.layers[i]
+    def ReLU_derivative(self, z):
+        return z > 0
+        
+    def backpropagation(self, layer_num, y, n_batches, alpha):
+        layer = self.layers[layer_num]
+        if(layer_num == len(self.layers) - 1):
+            dZ = layer.a - y 
+            dW = 1 / n_batches * np.dot(dZ, self.layers[layer_num - 1].a.T)
+            dB = 1 / n_batches * np.sum(dZ, axis=1, keepdims=True)
+            self.backpropagation(layer_num - 1, dZ, n_batches, alpha)
+        elif(layer_num == 0):
+            dZ = np.dot(self.layers[layer_num+1].weights.T, y) * self.ReLU_derivative(layer.z) 
+            dW = 1 / n_batches * np.dot(dZ, self.inputs.T)
+            dB = 1 / n_batches * np.sum(dZ, axis=1, keepdims=True)
+        else:       
+            dZ = np.dot(self.layers[layer_num+1].weights.T, y) * self.ReLU_derivative(layer.z) 
+            dW = 1 / n_batches * np.dot(dZ, self.layers[layer_num - 1].a.T)
+            dB = 1 / n_batches * np.sum(dZ, axis=1, keepdims=True)
+            self.backpropagation(layer_num - 1, dZ, n_batches, alpha)
+        self.adjust_params(layer, dW, dB, alpha)
+        
+    def adjust_params(self, layer, dW, dB, alpha):
+        layer.weights = layer.weights - alpha * dW
+        layer.biases = layer.biases - alpha * dB
 
 
 def one_hot(labels):
@@ -46,3 +74,6 @@ def one_hot(labels):
     one_hot_labels[np.arange(labels.size), labels] = 1
     one_hot_labels = one_hot_labels.T
     return one_hot_labels
+
+
+
