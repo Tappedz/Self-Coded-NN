34b2fe6a27fd5acaedef00b1318a630b371acc1e
Tappedz <80057934+Tappedz@users.noreply.github.com>
2024-01-10 16:18:48 +0100

    Train & test methods implemented

--- neural_network.py
+++ neural_network.py
@@ -18,10 +18,9 @@ class NeuralNetwork:
         self.layers.append(Layer(n_neurons, n_outputs, True))
 
     def forward(self, inputs): #inputs n_batches x n_inputs
-        self.inputs = inputs.T
         for i in range(len(self.layers)):
             if i == 0:
-                self.layers[i].forward(self.inputs)
+                self.layers[i].forward(inputs)
             else:
                 self.layers[i].forward(fwd)
             fwd = self.layers[i].a
@@ -46,34 +45,70 @@ class NeuralNetwork:
     def ReLU_derivative(self, z):
         return z > 0
         
-    def backpropagation(self, layer_num, y, n_batches, alpha):
+    def backpropagation(self, layer_num, inputs, y, batch_size, alpha):
         layer = self.layers[layer_num]
         if(layer_num == len(self.layers) - 1):
             dZ = layer.a - y 
-            dW = 1 / n_batches * np.dot(dZ, self.layers[layer_num - 1].a.T)
-            dB = 1 / n_batches * np.sum(dZ, axis=1, keepdims=True)
-            self.backpropagation(layer_num - 1, dZ, n_batches, alpha)
+            dW = 1 / batch_size * np.dot(dZ, self.layers[layer_num - 1].a.T)
+            dB = 1 / batch_size * np.sum(dZ, axis=1, keepdims=True)
+            self.backpropagation(layer_num - 1, inputs, dZ, batch_size, alpha)
         elif(layer_num == 0):
             dZ = np.dot(self.layers[layer_num+1].weights.T, y) * self.ReLU_derivative(layer.z) 
-            dW = 1 / n_batches * np.dot(dZ, self.inputs.T)
-            dB = 1 / n_batches * np.sum(dZ, axis=1, keepdims=True)
+            dW = 1 / batch_size * np.dot(dZ, inputs.T)
+            dB = 1 / batch_size * np.sum(dZ, axis=1, keepdims=True)
         else:       
             dZ = np.dot(self.layers[layer_num+1].weights.T, y) * self.ReLU_derivative(layer.z) 
-            dW = 1 / n_batches * np.dot(dZ, self.layers[layer_num - 1].a.T)
-            dB = 1 / n_batches * np.sum(dZ, axis=1, keepdims=True)
-            self.backpropagation(layer_num - 1, dZ, n_batches, alpha)
+            dW = 1 / batch_size * np.dot(dZ, self.layers[layer_num - 1].a.T)
+            dB = 1 / batch_size * np.sum(dZ, axis=1, keepdims=True)
+            self.backpropagation(layer_num - 1, inputs, dZ, batch_size, alpha)
         self.adjust_params(layer, dW, dB, alpha)
         
     def adjust_params(self, layer, dW, dB, alpha):
         layer.weights = layer.weights - alpha * dW
         layer.biases = layer.biases - alpha * dB
 
+    def one_hot(self, labels):
+        one_hot_labels = np.zeros((labels.size, labels.max() + 1))
+        one_hot_labels[np.arange(labels.size), labels] = 1
+        one_hot_labels = one_hot_labels.T
+        return one_hot_labels
+    
+    def train(self, X, y, alpha, batch_size):
+        inputs = X.T
+        y = self.one_hot(y)
+        acc = []
+        loss = []
+        if batch_size == 1:
+            inputs = np.array_split(inputs, inputs.shape[1], axis=1)
+            y = np.array_split(y, y.shape[1], axis=1)
+        else:
+            inputs = np.array_split(inputs, batch_size, axis=1)
+            y = np.array_split(y, batch_size, axis=1)
+        for i in range(len(inputs)):
+            pred = self.forward(inputs[i])
+            self.backpropagation(self.n_layers, y[i], batch_size, alpha)
+            acc.append(self.calculate_accuracy(pred, y))
+            loss.append(self.calculate_loss(pred, y))
+        return acc, loss
+    
+    def test(self, X, y, batch_size):
+        inputs = X.T
+        y = self.one_hot(y)
+        acc = []
+        loss = []
+        if batch_size == 1:
+            inputs = np.array_split(inputs, inputs.shape[1], axis=1)
+            y = np.array_split(y, y.shape[1], axis=1)
+        else:
+            inputs = np.array_split(inputs, batch_size, axis=1)
+            y = np.array_split(y, batch_size, axis=1)
+        for i in range(len(inputs)):
+            pred = self.forward(inputs[i])
+            acc.append(self.calculate_accuracy(pred, y))
+            loss.append(self.calculate_loss(pred, y))
+        return acc, loss
 
-def one_hot(labels):
-    one_hot_labels = np.zeros((labels.size, labels.max() + 1))
-    one_hot_labels[np.arange(labels.size), labels] = 1
-    one_hot_labels = one_hot_labels.T
-    return one_hot_labels
+
 
 
 
